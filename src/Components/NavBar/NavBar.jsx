c593129f0ce0f726427661bb402e46ca03837c41
Youssef Attea Tawfik <youssefattea1223@gmail.com>
2025-12-09 04:43:47 +0200

    commit

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
+  const [scroll, setScroll] = useState("p-4");
+
+  useEffect(() => {
+    function NavScroll() {
+      if (window.scrollY > 40) {
+        setScroll("p-1");
+      } else {
+        setScroll("p-4");
+      }
+    }
+
+    window.addEventListener("scroll", NavScroll);
+
+    return () => window.removeEventListener("scroll", NavScroll);
+  }, []);
+
   return (
-    // Nav Bar Tags
-    <Navbar expand="lg" className="bb p-4 z-1 position-fixed top-0 w-100   ">
+    <Navbar
+      expand="lg"
+      className={`bb ${scroll} z-1 position-fixed top-0 w-100 `}
+    >
       <Container>
         <Link
           className="text-white fw-bold text-decoration-none text-uppercase fs-1"
@@ -14,6 +33,7 @@ function NavBar() {
         >
           Start Framework
         </Link>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto d-flex gap-3 fs-5 list-unstyled  ">
@@ -22,7 +42,6 @@ function NavBar() {
                 "nav-link text-white fw-bold text-uppercase text-decoration-none " +
                 (isActive ? "active bg-success text-white rounded p-2 " : "")
               }
-              // className="text-white fw-bold text-uppercase text-decoration-none"
               to="About"
             >
               About
